069b17b41a9fc2dc33fe8739207d72d32328ac76
Louis Yuxu Lu <105407878+LouisYxLu@users.noreply.github.com>
2025-09-05 21:00:10 +0800

    Add files via upload

--- USRNet.py
+++ USRNet.py
@@ -19,7 +19,7 @@ class USRNet(nn.Module):
     def __init__(self):
         super(USRNet,self).__init__()
 
-        self.mns = MainNetworkStructure(3,8)
+        self.mns = MainNetworkStructure(3,16)
          
     def forward(self,x,T):
         
@@ -70,8 +70,8 @@ class RB_E(nn.Module):    #Residual Block for Encoder (RB_E)
         self.conv_2 = nn.Conv2d(channel,channel,kernel_size=3,stride=1,padding=1,bias=False)      
         self.conv_3 = nn.Conv2d(channel,channel,kernel_size=3,stride=1,padding=1,bias=False) 
                 
-        self.conv_h = nn.Conv2d(channel,channel,kernel_size=3,stride=1,padding=3,dilation=3,bias=False)  
-        self.conv_l = nn.Conv2d(channel,channel,kernel_size=3,stride=1,padding=3,dilation=3,bias=False) 
+        self.conv_h = nn.Conv2d(channel,channel,kernel_size=3,stride=1,padding=1,bias=False)  
+        self.conv_l = nn.Conv2d(channel,channel,kernel_size=3,stride=1,padding=1,bias=False) 
                                                  
         self.act = nn.PReLU(channel)
         self.norm = nn.GroupNorm(num_channels=channel, num_groups=1)
@@ -89,9 +89,8 @@ class RB_E(nn.Module):    #Residual Block for Encoder (RB_E)
         x_1 = self.act(self.norm(self.conv_1(x)))            
         
         x_lap = self.lap(x_1)
-        
         x_high = self.conv_h(x_lap)
-        x_low  = self.conv_l(x_1-x_lap)#self.conv_l(self.gau())               
+        x_low  = self.conv_l(x_1 - x_lap)#self.conv_l(self.gau())               
 
         x_2 = self.act(self.norm(self.conv_2(x_1 + x_high + x_low)))
                 
@@ -150,7 +149,7 @@ class Decoder(nn.Module):
         self.d1  = RB_D(channel*8)
         self.d2  = RB_D(channel*4)
         self.d3  = RB_D(channel*2)     
-        self.d4  = RB_D(channel*1)   
+        self.d4  = RB_D(channel)   
 
         self.conv_d1td2 = nn.Conv2d(8*channel,4*channel,kernel_size=1,stride=1,padding=0,bias=False)   
         self.conv_d2td3 = nn.Conv2d(4*channel,2*channel,kernel_size=1,stride=1,padding=0,bias=False)  
@@ -207,7 +206,7 @@ class NIL(nn.Module):    #Nodes Independent Learning and Inferring
         return xout
 
 class GlobalContextBlock(nn.Module):
-    def __init__(self, in_channels, reduction=4):
+    def __init__(self, in_channels, reduction=8):
         super(GlobalContextBlock, self).__init__()
         self.in_channels = in_channels
         self.reduction = reduction
@@ -259,7 +258,6 @@ class NILB(nn.Module):  # Nodes Independent Learning Block
         self.norm = nn.GroupNorm(num_channels=channel, num_groups=1)
 
     def forward(self, x):
-    
         x_1 = self.act(self.norm(self.conv_1(x)))
         x_1 = self.gc_block_1(x_1)  # Apply GCNet after the first convolution
 
